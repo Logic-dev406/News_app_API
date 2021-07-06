cbedea0d529dec1a1eee64894e01e7692ba4a93f
patrick sunday <sundayblinkz1@gmail.com>
2021-07-06 18:33:07 +0100

    put the comment code iside try and catch

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -5,44 +5,48 @@ const User = require('../models/user');
 
 class CommentsController {
     static async createComment(req, res) {
-        // find out which post you are commenting
-        const id = req.params.id;
+        try {
+            // find out which post you are commenting
+            const id = req.params.id;
 
-        const postComments = await News.findById(id)
-            .populate({
-                path: 'comments',
-                model: 'Comment',
-            })
-            .select('comments');
+            const postComments = await News.findById(id)
+                .populate({
+                    path: 'comments',
+                    model: 'Comment',
+                })
+                .select('comments');
 
-        const userCommented =
-            postComments.comments.lenght < 1
-                ? false
-                : postComments.comments.find(
-                      (comment) => comment.user == req.body.user
-                  );
+            const userCommented =
+                postComments.comments.lenght < 1
+                    ? false
+                    : postComments.comments.find(
+                          (comment) => comment.user == req.body.user
+                      );
 
-        if (userCommented) {
-            return res
-                .status(400)
-                .send(response('User already commented', {}, false));
+            if (userCommented) {
+                return res
+                    .status(400)
+                    .send(response('User already commented', {}, false));
+            }
+
+            // get the comment text and record post id
+            const comment = new Comment({
+                comment: req.body.comment,
+                news: id,
+                user: req.body.user,
+            });
+            // save comment
+            await comment.save();
+            // get this particular post
+            const postRelated = await News.findById(id);
+            // push the comment into the post.comments array
+            postRelated.comments.push(comment);
+            // save and redirect...
+            await postRelated.save();
+            res.status(200).send(postRelated);
+        } catch (err) {
+            console.log(err);
         }
-
-        // get the comment text and record post id
-        const comment = new Comment({
-            comment: req.body.comment,
-            news: id,
-            user: req.body.user,
-        });
-        // save comment
-        await comment.save();
-        // get this particular post
-        const postRelated = await News.findById(id);
-        // push the comment into the post.comments array
-        postRelated.comments.push(comment);
-        // save and redirect...
-        await postRelated.save();
-        res.status(200).send(postRelated);
     }
 }
 
