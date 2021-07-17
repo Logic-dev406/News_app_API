f9260d38e6a2dab52cfac4a0068b30e06bd03511
patrick sunday <sundayblinkz1@gmail.com>
2021-07-17 20:14:48 +0100

    updated the get user by id endpoint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -24,7 +24,7 @@ class UsersController {
     static async getUserById(req, res) {
         const user = await User.findById(req.params.id).select('-passwordHash');
 
-        if (!user || !user.status === 'active') {
+        if (!user || user.status === 'not-active') {
             return res.status(500).send(response('user not found', {}, false));
         }
 
